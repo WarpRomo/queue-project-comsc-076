16b13da40986a3f42109232fac27c386a8f7f5b3
Ritik <kooltyme1@gmail.com>
2024-07-23 15:18:37 -0700

    Add files via upload

--- CheckoutStation.java
+++ CheckoutStation.java
@@ -1,11 +1,21 @@
+/*
+ * Preston Hsiao, Ritik Jalisatgi, Maria Roceo Sy, Saathvika Sripathi
+ * July 24, 2024
+ * 
+ * This program creates a Customer object, assigns a Customer to the
+ * station, helps the Customer to check out items, and removes the
+ * Customer from the station when all the items are checked out.
+ */
+
 public class CheckoutStation {
 
     Customer customer = null;
-
+	//Constructor
     public CheckoutStation(){
 
     }
-
+	//If the Customer has checked out all their items, remove the
+	//Customer from the station
     public Customer checkoutTick(){
 
         if(this.customer == null){
@@ -31,7 +41,7 @@ public class CheckoutStation {
 
 
     }
-
+	//Assign a Customer to the station
     public void setCustomer(Customer customer){
         this.customer = customer;
     }
